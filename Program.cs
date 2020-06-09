db6e1a21e181242b58a2ac2a5a7041fe353b0106
nagireddy panditi <nagireddy.panditi@gmail.com>
2020-06-09 18:17:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace web_app_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
